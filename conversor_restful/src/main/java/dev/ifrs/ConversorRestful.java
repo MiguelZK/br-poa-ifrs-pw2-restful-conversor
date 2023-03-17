3a96d38622b224db77c1bcc48670b2acd483febe
Miguel Zanona Krasner <miguelzkestudos@gmail.com>
2023-03-16 21:33:15 -0300

    Completado - c/ ajuda do Roberto e da Taíse

--- conversor_restful/src/main/java/dev/ifrs/ConversorRestful.java
+++ conversor_restful/src/main/java/dev/ifrs/ConversorRestful.java
@@ -1,10 +1,11 @@
 package dev.ifrs;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,14 +16,19 @@ public class ConversorRestful {
      * @param quilometros
      * @return
      */
-    @Path("/quilo_milhas/quilometros")
+    @Path("/quilometros")
     @POST
-    @Consumes(MediaType.APLICATION_FORM_URLENCODED)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_PLAIN)
-    public Double milhas(@PathParam("quilometros") String quilometros) {
-        
-        Double quiloms = Double.parseDouble(quilometros);
+    public String milhas(@FormParam("quilometros") double quilometros) {
 
-        return String.valueOf(quiloms * 0.621371);
+        return String.valueOf(quilometros * 0.621371);
+    }
+
+    @Path("/{nos}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ConversorNosEmKm conversorNosEmKm(@PathParam("nos") double nos) {
+        return new ConversorNosEmKm(nos);
     }
 }
